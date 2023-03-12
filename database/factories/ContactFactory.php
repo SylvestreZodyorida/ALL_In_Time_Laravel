a76d12f5abbb22141b8b97702f2bad05e77aee2f
SylvestreZodyorida <sylvestrea863@gmail.com>
2023-03-12 11:05:07 +0100

    second commit

---
+++ database/factories/ContactFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ContactFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->name(),
+            'surname' => fake()->surname(),
+            'email' => fake()->email(),
+            'numero' => fake()->numero(),
+            'jour' => fake()->jour(),
+            'mois' => fake()->mois(),
+            'annee' => fake()->annee(),
+        ];
+    }
+}
---
+++ resources/views/resultatContact.blade.php
@@ -0,0 +1,12 @@
+@extends("layouts.master")
+
+@section("contenu")
+<br><br><br><br><br><br>
+<table class="container table"> 
+    <tbody>
+        
+        <tr>
+           rurururur
+        </tr>
+    </tbody>
+</table>
